ec91a373667c8efa7044ad78bd305cb398a42136
HNHI133 <vuhoangnhi1303@gmail.com>
2024-12-11 21:10:42 +0700

    Update MainUI.java

--- source/java/elgamal_java_project/src/main/java/com/mycompany/elgamal_java_project/view/MainUI.java
+++ source/java/elgamal_java_project/src/main/java/com/mycompany/elgamal_java_project/view/MainUI.java
@@ -1,10 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.elgamal_java_project.view;
-
-
-public class MainUI {
-    
-}
+public static void main(String[] args) {
+        ElGamalScreen view = new ElGamalScreen();
+        ElGamalController controller = new ElGamalController(view);
+    } dùng thì gọi như này
