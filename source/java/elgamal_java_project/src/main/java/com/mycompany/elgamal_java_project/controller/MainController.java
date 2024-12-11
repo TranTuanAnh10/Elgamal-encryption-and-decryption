29a7e0fa10b43de4417e2afe1fed772d156b86fc
HNHI133 <vuhoangnhi1303@gmail.com>
2024-12-11 21:19:18 +0700

    Update MainController.java

--- source/java/elgamal_java_project/src/main/java/com/mycompany/elgamal_java_project/controller/MainController.java
+++ source/java/elgamal_java_project/src/main/java/com/mycompany/elgamal_java_project/controller/MainController.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.elgamal_java_project.controller;
-
-public class MainController {
-    
-}
+public static void main(String[] args) {
+        ElGamalScreen view = new ElGamalScreen();
+        ElGamalController controller = new ElGamalController(view);
+    } dùng thì gọi như này
