9d18aac7366942c62a51442942799580b6a72403
HNHI133 <vuhoangnhi1303@gmail.com>
2024-12-11 21:16:52 +0700

    Delete source/java/elgamal_java_project/src/main/java/com/mycompany/elgamal_java_project/view

--- source/java/elgamal_java_project/src/main/java/com/mycompany/elgamal_java_project/view/MainUI.java
+++
@@ -1,4 +0,0 @@
-public static void main(String[] args) {
-        ElGamalScreen view = new ElGamalScreen();
-        ElGamalController controller = new ElGamalController(view);
-    } dùng thì gọi như này
