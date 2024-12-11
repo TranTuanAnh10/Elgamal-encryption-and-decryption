b5c8fd76076171417cafc1d9980405201512a241
HNHI133 <vuhoangnhi1303@gmail.com>
2024-12-11 21:09:44 +0700

    Add files via upload

---
+++ source/java/elgamal_java_project/src/main/java/com/mycompany/elgamal_java_project/controller/ElGamalController.java
@@ -0,0 +1,70 @@
+package com.mycompany.elgamal_java_project.controller;
+
+import com.mycompany.elgamal_java_project.view.ElGamalScreen;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ElGamalController {
+    private ElGamalScreen view;
+
+    public ElGamalController(ElGamalScreen view) {
+        this.view = view;
+
+        // Thêm các sự kiện cho các nút
+        this.view.addSaveKeyListener(new SaveKeyListener());
+        this.view.addCreateKeyListener(new CreateKeyListener());
+        this.view.addRandomKeyListener(new RandomKeyListener());
+        this.view.addEncryptButtonListener(new EncryptButtonListener());
+        this.view.addDecodeButtonListener(new DecodeButtonListener());
+    }
+
+    // Lớp sự kiện cho SaveKeyButton
+    class SaveKeyListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Xử lý khi nhấn Save Key
+            System.out.println("Save Key");
+        }
+    }
+
+    // Lớp sự kiện cho CreateKeyButton
+    class CreateKeyListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Xử lý khi nhấn Create Key
+            System.out.println("Create Key");
+
+        }
+    }
+
+    // Lớp sự kiện cho RandomKeyButton
+    class RandomKeyListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Xử lý khi nhấn Random Key
+            System.out.println("Random Key");
+
+        }
+    }
+
+    // Lớp sự kiện cho EncryptButton
+    class EncryptButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Xử lý khi nhấn EncryptButton
+            System.out.println("EncryptButton");
+
+        }
+    }
+
+    // Lớp sự kiện cho DecodeButton
+    class DecodeButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Xử lý khi nhấn DecodeButton
+            System.out.println("DecodeButton");
+
+        }
+    }
+}
